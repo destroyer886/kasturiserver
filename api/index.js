9cb22b0ce16300baf472e151893719e302e16e33
dhruv chaudhary <dhruvchoudhary88649@gmail.com>
2024-07-04 00:24:42 +0530

    api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const app = require('../server');
+const {connectToDb} = require('../db')
 
 
 module.exports = app;
+module.exports = async (req, res) => {
+    try {
+    connectToDb();
+      res.status(200).json({ message: 'Connected to MongoDB' });
+    } catch (error) {
+      console.error('Error connecting to MongoDB', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
 
+  connectToDb()

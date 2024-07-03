9440eb3b978a6aa168c08991610a5baeacd518bd
ThunderCoder <134991440+destroyer886@users.noreply.github.com>
2024-07-04 00:36:49 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ module.exports = app;
 module.exports = async (req, res) => {
     try {
     connectToDb();
-      res.status(200).json({ message: 'Connected to MongoDB' });
+      res.status(200).json({ message: 'Server is Running' });
     } catch (error) {
       console.error('Error connecting to MongoDB', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   };
 
-  connectToDb()
+  
